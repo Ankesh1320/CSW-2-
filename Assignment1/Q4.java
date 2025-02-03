9779c638424c5072af678a97c7f2652fe9186d1f
Ankesh1320 <145012513+Ankesh1320@users.noreply.github.com>
2025-02-03 16:00:31 +0530

    Q4.java

--- Assignment1/Q4.java
+++ Assignment1/Q4.java
@@ -3,15 +3,22 @@
 // method. Create the object of the image class using the default and parameterized 
 // constructor and print the details of the object.
 class Image{
-    private int imageWidth;
-    private int imageHeight;
-    private String colorCode;
+     int imageWidth;
+     int imageHeight;
+     String colorCode;
     public Image(){
-        this.imageWidth=imageWidth;
-        this.imageHeight=imageHeight;
-        this.colorCode=colorCode;
+        this.imageWidth=0;
+        this.imageHeight=0;
+        this.colorCode=null;
 
 }
+
+    public Image(int imageWidth, int imageHeight, String colorCode) {
+        this.imageWidth = imageWidth;
+        this.imageHeight = imageHeight;
+        this.colorCode = colorCode;
+    }
+    
 public void setImageWidth(int imageWidth) {
     this.imageWidth = imageWidth;
     }
@@ -39,8 +46,8 @@ public class Q4 {
     
         Image image = new Image();
         System.out.println(image.toString());
-        image.setImageWidth(100);
-        image.setImageHeight(200);
+        image.setImageWidth(1000);
+        image.setImageHeight(2001);
         image.setColorCode("Red");
         System.out.println(image.toString());
         
